a4586b586c8a55e56e0bab8f35c4d59435a7fcc3
Ryan Waits <ryanwaits@gmail.com>
2018-05-22 11:29:43 -0500

    update filter method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,11 @@ class DashboardController < ApplicationController
   def index
     response = Faraday.get 'https://venmo.com/api/v5/public'
     feed = JSON.parse(response.body)['data']
-    @feed = filter_by_keyword(feed)
+    if filter_by_keyword(feed).empty?
+      @feed = feed
+    else 
+      @feed = filter_by_keyword(feed)
+    end
   end
 
   private
